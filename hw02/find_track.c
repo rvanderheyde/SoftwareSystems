6d62ce03c8e3ae9410b565ce4906aa8fc78877e0
Radmer van der Heyde <radmer.vanderheyde@students.olin.edu>
2015-10-11 19:00:57 -0400

    regex version of find_track

--- hw02/find_track.c
+++ hw02/find_track.c
@@ -8,6 +8,7 @@ License: Creative Commons Attribution-ShareAlike 3.0
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -39,6 +40,21 @@ void find_track(char search_for[])
 void find_track_regex(char pattern[])
 {
     // TODO: fill this in
+    regex_t regex;
+    int reti;
+
+    reti = regcomp(&regex, pattern, 0);
+    if (reti) {
+        fprintf(stderr, "Could not compile regex\n");
+        exit(1);
+    }
+
+    int i;
+    for (i=0; i<NUM_TRACKS; i++) {
+        reti = regexec(&regex, tracks[i], 0, NULL, 0);
+        if (!reti)
+            printf("Track %i: '%s'\n", i, tracks[i]);
+    }
 }
 
 // Truncates the string at the first newline, if there is one.
